bbc4f33a56d4c920653eec0424fac613e7639ce6
Angela Griffin <Angela.Griffin@bcbsnc.com>
2016-10-31 16:53:56 -0400

    Draw tictak board

---
+++ tictacktoe/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tictacktoe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array1 = new string[11];
+            array1[0] = "   |   |   ";
+            string[] array2 = new string[11];
+            array2[0] = "---|---|---";
+
+            for (int i = 0; i < 11; i = i + 1)
+            {
+                Console.WriteLine(array1[i]);
+                Console.WriteLine(array2[i]);
+                Console.WriteLine(array1[i]);
+                Console.WriteLine(array2[i]);
+                Console.WriteLine(array1[i]);
+                
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
